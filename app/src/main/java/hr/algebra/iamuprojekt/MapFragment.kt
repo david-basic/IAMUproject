cf3736dffbbf40f956303ef42c784fbdecfd1ddf
David Bašić <d.basic97.db@gmail.com>
2023-05-31 16:37:33 +0200

    google maps added and working

--- app/src/main/java/hr/algebra/iamuprojekt/MapFragment.kt
+++ app/src/main/java/hr/algebra/iamuprojekt/MapFragment.kt
@@ -1,22 +1,38 @@
 package hr.algebra.iamuprojekt
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 class MapFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val rijeka = LatLng(45.3271, 14.4422)
+        googleMap.addMarker(MarkerOptions().position(rijeka).title("Marker in Rijeka"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rijeka, 13f))
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 }
